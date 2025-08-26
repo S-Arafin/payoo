766956dcb2f387046bb754aa2aebf08854aa4eb8
S-Arafin <arafin23103@gmail.com>
2025-08-26 22:01:25 +0600

    add login page with js funtionalities

--- login.js
+++ login.js
@@ -1,5 +1,15 @@
 // login functionality 
-document.getElementById('loginBtn').addEventListener('click', function(event){
+document.getElementById('loginBtn').addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('clk');
+    const mobileNumber = '01979817736';
+    const pinNumber = '2310';
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+    const pinValue = document.getElementById('pin').value
+    if (mobileNumberValue === mobileNumber && pinValue === pinNumber) {
+        window.location.href = "home.html"
+    }
+    else {
+        alert("invalid credentials")
+    }
+
 })
